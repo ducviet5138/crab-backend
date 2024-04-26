db8a420228e326130d9995fc38049ccaf40f906c
ducviet5138 <ducviet5138@gmail.com>
2024-04-26 10:30:36 +0700

    CRAB-2 Feat: Init backend with bucket

---
+++ src/app-data-src.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+export default async function databaseInitialize(){
+    const uri = 'mongodb://127.0.0.1:27017/crab';
+    console.log('[Database] Connecting to database...');
+    try {
+        await mongoose.connect(uri);
+        console.log('[Database] Database has been initialized!');
+    } catch (err: any) {
+        console.error('[Database] Error: ', err);
+    }
+}
